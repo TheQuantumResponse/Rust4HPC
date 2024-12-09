fdbf3eee5a8b52b5f2297b005cbb54fc0214ea89
The Quantum Response <thequantumresponse@gmail.com>
2024-12-09 15:51:41 -0500

    Added ndarray mm bench.

--- matrix_operations/matrix_multiplication/benches/benchmark_matrix_multiplication.rs
+++ matrix_operations/matrix_multiplication/benches/benchmark_matrix_multiplication.rs
@@ -3,6 +3,8 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use matrix::Matrix;
 use matrix_multiplication::matrix_multiplication;
 
+use ndarray::{Array, Ix2};
+
 fn benchmark_matrix_multiplication(crit: &mut Criterion) {
     let m = 1000;
     let n = 1000;
@@ -17,13 +19,37 @@ fn benchmark_matrix_multiplication(crit: &mut Criterion) {
     group.measurement_time(std::time::Duration::new(10, 0));
     group.sample_size(10);
 
-    group.bench_function("Matrix::set", |block| {
+    group.bench_function("matrix_multiplication", |block| {
         block.iter(|| {
             matrix_multiplication(black_box(&mut c), black_box(&a), black_box(&b)).unwrap();
         })
     });
-        
+
     group.finish();
 }
-criterion_group!(benches, benchmark_matrix_multiplication);
+
+fn benchmark_ndarray(crit: &mut Criterion) {
+    let m = 1000;
+    let n = 1000;
+    let k = 1000;
+    let initial_value = 2.5f64;
+
+    let mut c = Array::<f64, Ix2>::from_elem((m,n), 0.0f64);
+    let a = Array::<f64, Ix2>::from_elem((m,k), initial_value);
+    let b = Array::<f64, Ix2>::from_elem((k,n), initial_value);
+
+    let mut group = crit.benchmark_group("ndarray-1000x1000x1000");
+    group.measurement_time(std::time::Duration::new(10, 0));
+    group.sample_size(10);
+
+    group.bench_function("ndarray::dot", |block| {
+        block.iter(|| {
+            c = a.dot(&b);
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, benchmark_matrix_multiplication, benchmark_ndarray);
 criterion_main!(benches);
